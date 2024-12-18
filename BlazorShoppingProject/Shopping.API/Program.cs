ef87e41e7336a6ad5d22e17fc80e8e587377ab00
Yutthana Tantinimitchok <tan@inspectrealestate.com.au>
2024-12-19 00:04:46 +1000

    Create Database  with entity frame work

--- BlazorShoppingProject/Shopping.API/Program.cs
+++ BlazorShoppingProject/Shopping.API/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using Shopping.API.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+// Register the DbContext with the dependency injection container
+builder.Services.AddDbContext<ShopOnlineDBContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
