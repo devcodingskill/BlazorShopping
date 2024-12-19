716b0e82edc9e904bd2e93db839a38629c5a2083
Yutthana Tantinimitchok <tan@inspectrealestate.com.au>
2024-12-19 23:54:04 +1000

    Add Dto models

---
+++ BlazorShoppingProject/Shopping.Models/Dtos/CartAItemDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shopping.Models.Dtos
+{
+    public class CartAItemDto
+    {
+        public int Id { get; set; }
+        public int CartId { get; set; }
+        public int ProductId { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+        public string ProductName { get; set; }
+        public string ProductDescription { get; set; } = string.Empty;
+        public string ProductImageURL { get; set; }
+        public decimal TotalPrice { get; set; } 
+    }
+}
---
+++ BlazorShoppingProject/Shopping.Models/Dtos/ProductDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shopping.Models.Dtos
+{
+    public class ProductDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+        public string ImageURL { get; set; }
+        public int Quantity { get; set; }
+        public string CategoryName { get; set; }
+        public string CategoryId { get; set; }
+
+    }
+}
