13534455fb044980e60b7c31213f2a8c605238db
Yutthana Tantinimitchok <tan@inspectrealestate.com.au>
2024-12-20 00:14:46 +1000

    Add Repository factory in the API

---
+++ BlazorShoppingProject/Shopping.API/Repository/Contacts/IProductRepository.cs
@@ -0,0 +1,12 @@
+﻿using Shopping.API.Entities;
+
+namespace Shopping.API.Repository.Contacts
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetItems();
+        Task<IEnumerable<ProductCategory>> GetCategories(); 
+        Task<ProductCategory> GetCategory(int id);
+        Task<Product> GetItem(int id);
+    }
+}
---
+++ BlazorShoppingProject/Shopping.API/Repository/ProductRepository.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Shopping.API.Data;
+using Shopping.API.Entities;
+using Shopping.API.Repository.Contacts;
+
+namespace Shopping.API.Repository
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly ShopOnlineDBContext _shopOnlineDBContext;
+
+        public ProductRepository(ShopOnlineDBContext shopOnlineDBContext)
+        {
+         _shopOnlineDBContext = shopOnlineDBContext;   
+        }
+        public async Task<IEnumerable<ProductCategory>> GetCategories()
+        {
+            var categories = await _shopOnlineDBContext.ProductCategories.ToListAsync();
+            return categories;
+        }
+
+        public Task<ProductCategory> GetCategory(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<Product>> GetItems()
+        {
+            var products = await _shopOnlineDBContext.Products.ToListAsync();
+            return products;
+        }
+
+        public Task<Product> GetItem(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
