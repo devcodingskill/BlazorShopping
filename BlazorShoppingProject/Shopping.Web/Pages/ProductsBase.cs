38fe70cf86ecea8974345ff1dcee2b0a70e8450b
Yutthana Tantinimitchok <tan@inspectrealestate.com.au>
2024-12-20 21:22:04 +1000

    Clean up and add comment

--- BlazorShoppingProject/Shopping.Web/Pages/ProductsBase.cs
+++ BlazorShoppingProject/Shopping.Web/Pages/ProductsBase.cs
@@ -4,12 +4,16 @@ using Shopping.Web.Services.Contracts;
 
 namespace Shopping.Web.Pages
 {
+    //This class need to be inherited by other components
     public class ProductsBase : ComponentBase
     {
+        //Inject the ProductService to get the products
         [Inject]
         public IProductService ProductService { get; set; }
         public IEnumerable<ProductDto> Products { get; set; }
 
+        //We need to override the OnInitializedAsync method to get the products
+        //this is a lifecycle method for blazor that is called when the component is initialized
         override protected async Task OnInitializedAsync()
         {
             Products = await ProductService.GetItems();
