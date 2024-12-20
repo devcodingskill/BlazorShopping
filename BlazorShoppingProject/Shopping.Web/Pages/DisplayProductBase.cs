0d92f903586c7607608901e03d9117590d8287e9
Yutthana Tantinimitchok <tan@inspectrealestate.com.au>
2024-12-20 21:57:54 +1000

    Create the child component for display product

---
+++ BlazorShoppingProject/Shopping.Web/Pages/DisplayProductBase.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Components;
+using Shopping.Models.Dtos;
+
+namespace Shopping.Web.Pages
+{
+    public class DisplayProductBase : ComponentBase
+    {
+        [Parameter]
+        public IEnumerable<ProductDto> Products { get; set; }
+
+    }
+}
